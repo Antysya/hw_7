a52970cf1db37bb615050ef381b32562b89da624
Пользователь <@DESKTOP-6V93RUO>
2022-09-21 10:53:30 +0300

    Добавьте файлы проекта.

---
+++ hw_7/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw_7
+{
+    internal class Program
+    {
+        
+        public class My_Exep : ApplicationException
+        {
+            public My_Exep() : base("Стоимость автомобиля завышена!!!") { }
+            
+        }
+
+        public static AutoPas f(int _size)
+        {
+           return new AutoPas(_size);
+        }
+        public static AutoPas f2(AutoPas _autoPas)
+        {
+            _autoPas[0] = new Passenger { Stamp = "Volvo", Model = "XC90", Cost = 4100000, Type = "Кроссовер", PassengerNumbers = 5 };
+            _autoPas[1] = new Passenger { Stamp = "KIA", Model = "Optima", Cost = 3500000, Type = "Седан", PassengerNumbers = 5 };
+            _autoPas[2] = new Passenger { Stamp = "Audi", Model = "TT", Cost = 5300000, Type = "Спорткар", PassengerNumbers = 2 };
+            return _autoPas;
+        }
+        public static void f3(AutoPas _autoPas)
+        {
+         
+            for (int i = 0; i < _autoPas.Length; i++)
+            {
+                WriteLine(_autoPas[i]);
+            }
+        }
+        public static void f4(AutoPas _autoPas)
+        {
+            try
+            {
+                WriteLine("***Мои исключения из функции***");
+                for (int i = 0; i < _autoPas.Length; i++)
+                {
+                    if (_autoPas[i].Cost > 5000000)
+                    {
+                        WriteLine(_autoPas[i]);
+                        throw new My_Exep();
+                    }
+                    else
+                        WriteLine(_autoPas[i]);
+                }
+            }
+            catch (My_Exep e)
+            {
+                WriteLine($"{e.Message}");
+            }
+            finally
+            {
+                WriteLine("Выход из функции");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            AutoPas autoPas = new AutoPas(3);
+            autoPas[0] = new Passenger { Stamp = "Volvo", Model = "XC90", Cost = 4100000, Type = "Кроссовер", PassengerNumbers = 5 };
+            autoPas[1] = new Passenger { Stamp = "KIA", Model = "Optima", Cost = 3500000, Type = "Седан", PassengerNumbers = 5 };
+            autoPas[2] = new Passenger { Stamp = "Audi", Model = "TT", Cost = 5300000, Type = "Спорткар", PassengerNumbers = 2 };
+
+            AutoPas autoPas2 = new AutoPas(4);
+            autoPas2[0] = new Passenger { Stamp = "Volvo", Model = "XC90", Cost = 4100000, Type = "Кроссовер", PassengerNumbers = 5 };
+            autoPas2[1] = new Passenger { Stamp = "KIA", Model = "Optima", Cost = 3500000, Type = "Седан", PassengerNumbers = 5 };
+            autoPas2[2] = new Passenger { Stamp = "Audi", Model = "TT", Cost = 5300000, Type = "Спорткар", PassengerNumbers = 2 };
+
+            AutoC autoC = new AutoC(3);
+            autoC[0] = new Cargo { Stamp = "Hyundai", Model = "HD78", Cost = 990000000, Type = "Рефрижиратор", Capacity = 5 };
+            autoC[1] = new Cargo { Stamp = "MAZ", Model = "TF45", Cost = 875000000, Type = "Самосвал", Capacity = 10 };
+            autoC[2] = new Cargo { Stamp = "Hyundai", Model = "HD79", Cost = 923000000, Type = "Фургон", Capacity = 6 };
+
+
+            try
+            {
+                WriteLine("***ПРОВЕРКА СТОИМОСТИ***");
+                for (int i = 0; i < autoPas.Length; i++)
+                {
+                    if (autoPas[i].Cost > 5000000)
+                    {
+                        WriteLine(autoPas[i]);
+                        throw new My_Exep();
+                    }
+                    else
+                        WriteLine(autoPas[i]);
+                }
+            }
+            catch (My_Exep e)
+            {
+                WriteLine($"{e.Message}");
+            }
+            finally
+            {
+                WriteLine("**********************");
+            }
+
+            try
+            {
+                WriteLine("***ПРОВЕРКА СТОИМОСТИ***");
+                f4(autoPas);
+            }
+            catch (My_Exep e)
+            {
+                WriteLine($"{e.Message}");
+            }
+            finally
+            {
+                WriteLine("**********************");
+            }
+
+            try
+            {
+                WriteLine("***Проверка пределов массива***");
+                double sum = 0;
+                for (int i = 0; i < autoPas2.Length; i++)
+                {
+                    sum += autoPas2[i].Cost;
+                }
+                WriteLine($"Стоимость пассажирского транспорта = {sum}\n");
+            }
+            catch (Exception e)
+            {
+                WriteLine(e.Message);
+            }
+            finally
+            {
+                WriteLine("**********************");
+            }
+
+            try
+            {
+                WriteLine("***Stack trace и Target Site***");
+                WriteLine("***Вызов без ошибок:***");
+                f3(f2(f(3)));
+                WriteLine("***Вызов с ошибкой:***");
+                f3(f2(f(2)));
+            }
+            catch (Exception e)
+            {
+                WriteLine($"Stack trace: \n {e.StackTrace}");
+                WriteLine($"Target Site: \n {e.TargetSite}");
+            }
+            finally
+            {
+                WriteLine("**********************");
+            }
+
+            try
+            {
+                WriteLine("***Checked при выполнении арифметической операции (double -> int)***");
+               double sum = 0;
+                for (int i = 0; i < autoC.Length; i++)
+                {
+                    sum += autoC[i].Cost;
+                }
+                WriteLine($"Стоимость грузового транспорта = {checked((int)sum)}\n");
+            }
+            catch (OverflowException e)
+            {
+                WriteLine(e.Message);
+            }
+            finally
+            {
+                WriteLine("**********************");
+            }
+        }
+    }
+}
---
+++ hw_7/TC.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace hw_7
+{
+    abstract class TC
+    {
+        public string Stamp { get; set; }
+        public string Model { get; set; }
+        public int Cost { get; set; }
+        public override string ToString()
+        {
+            return $"\nМарка: {Stamp} Модель: {Model} Стоимость: {Cost} руб.";
+        }
+    }
+
+    abstract class Ground : TC
+    {
+        public string Type { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $" Тип: {Type}";
+        }
+    }
+    abstract class Air : TC
+    {
+        public int NumberEngines { get; set; }
+        public override string ToString()
+        {
+            return base.ToString() + $" Вид: {NumberEngines}";
+        }
+    }
+
+    interface IFunctions
+    {
+        string IsWork();
+        string Work();
+        string Transfer(); 
+    }
+
+    interface IPark
+    {
+        List<IFunctions> park { get; set; }
+        int Costs();
+    }
+
+    class Passenger : Ground, IFunctions
+    {
+        public int PassengerNumbers { get; set; }
+        public string IsWork()
+        {
+            return "Наземный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Перевожу пассажиров. ";
+        }
+        public string Work()
+        {
+            return "Легковой. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Количество пассажиров: {PassengerNumbers} чел.";
+        }
+    }
+
+    class Cargo : Ground, IFunctions
+    {
+        public int Capacity { get; set; }
+        public string IsWork()
+        {
+            return "Наземный транспорт";
+        }
+        public string Transfer()
+        {
+            return "Перевожу грузы. ";
+        }
+        public string Work()
+        {
+            return "Грузовой. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Грузоподъемность: {Capacity} т";
+        }
+
+    }
+
+    class Moto : Ground, IFunctions
+    {
+        public int Power { get; set; }
+
+        public string IsWork()
+        {
+            return "Наземный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Катаю по красивым местам. ";
+        }
+        public string Work()
+        {
+            return "Мотоцикл. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Мощность: {Power} л.с.";
+        }
+    }
+
+    class Plane : Air, IFunctions
+    {
+        public int Distance { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Перевожу пассажиров на другие континенты. ";
+        }
+        public string Work()
+        {
+            return "Самолет. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Дальность: {Distance} км";
+        }
+
+    }
+
+    class Helicopter : Air, IFunctions
+    {
+        public int NumberScrews { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Перевожу грузы на другие континенты. ";
+        }
+        public string Work()
+        {
+            return "Вертолет. ";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" Количество несущих винтов: {NumberScrews}";
+        }
+    }
+
+    class Airship : Air, IFunctions
+    {
+        public string FillingGas { get; set; }
+        public string IsWork()
+        {
+            return "Воздушный транспорт";
+        }
+
+        public string Transfer()
+        {
+            return "Несу рекламу на борту. ";
+        }
+        public string Work()
+        {
+            return "Дирижабль. ";
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Заполняющий газ: {FillingGas}";
+        }
+    }
+
+    class Park : TC, IPark
+    {
+        public List<IFunctions> park { get; set; }
+
+        int IPark.Costs()
+        {
+            int sum = 0;
+            foreach (TC item in park)
+            {
+                sum += item.Cost;
+            }
+            return sum;
+        }
+    }
+
+    class AutoPas
+    {
+        Passenger[] parks;
+        public AutoPas(int size)
+        {
+            parks = new Passenger[size];
+        }
+        public int Length { get { return parks.Length; } }
+        public Passenger this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < parks.Length)
+                {
+                    return parks[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                parks[index] = value;
+            }
+        }
+    }
+
+    class AutoC
+    {
+        Cargo[] parks;
+        public AutoC(int size)
+        {
+            parks = new Cargo[size];
+        }
+        public int Length { get { return parks.Length; } }
+        public Cargo this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < parks.Length)
+                {
+                    return parks[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                parks[index] = value;
+            }
+        }
+
+    }
+}
